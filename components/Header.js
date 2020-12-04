3b790ee824d7574d80b2eba30939d047a9aff2a4
jane_moody <janeemoody2@gmail.com>
2020-12-04 13:07:23 -0500

    Finished tabs.js

--- components/Header.js
+++ components/Header.js
@@ -10,21 +10,27 @@
 //
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
+const headContainer = document.querySelector(".header-container");
 
 function Header() {
-  const header = document.createElement("header");
+  const header = document.createElement("div");
   const date = document.createElement("span");
   const h1 = document.createElement("h1");
   const temp = document.createElement("span");
 
+  date.textContent = "MARCH 28, 2020";
+  h1.textContent = "Lambda Times";
+  temp.textContent = "98°";
+
   header.classList.add("header");
   date.classList.add("date");
-  h1.classList.add("h1");
   temp.classList.add("temp");
 
+  header.append(h1, date, temp);
   header.appendChild(date);
   header.appendChild(h1);
   header.appendChild(temp);
 
   return header;
 }
+headContainer.append(Header());
